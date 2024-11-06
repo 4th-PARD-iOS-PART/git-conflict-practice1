cff1d785f17630d103bcb863454110eb076e6065
김사랑 <love0l123416@handong.ac.kr>
2024-11-06 21:19:07 +0900

    C 수정

--- practice1/practice1/ViewController.swift
+++ practice1/practice1/ViewController.swift
@@ -1,21 +1,30 @@
 // ViewController.swift
 import UIKit
+
 class ViewController: UIViewController {
+    // mainLabel 부분만 수정하세요!
     private let mainLabel: UILabel = {
-let label = UILabel()
-label.text = " "
-label.textAlignment = .center
-label.font = .systemFont(ofSize: 24) label.translatesAutoresizingMaskIntoConstraints = fal return label
-}()
+        let label = UILabel()
+        label.text = "C의 라벨입니다"  // 이 부분을 수정
+        label.textAlignment = .center
+        label.font = .italicSystemFont(ofSize: 35)  // 이 부분을 수정
+        label.textColor = .green  // 이 부분을 추가
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupUI()
-}
+    }
+    
     private func setupUI() {
         view.addSubview(mainLabel)
+        
         NSLayoutConstraint.activate([
-            mainLabel.centerXAnchor.constraint(equalTo: view.
-            mainLabel.centerYAnchor.constraint(equalTo: view.
-]) }
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
